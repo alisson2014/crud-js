10939dceb18d35022de8c25624f9777bf7a52196
alisson2014 <almeidaalisson2014@gmail.com>
2023-01-15 20:42:18 -0300

    terminei o script

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const btnSave = document.querySelector("button#save")
+const btnCancel = document.querySelector("button#cancel")
 //CRUD - create read update delete
 const getLocalStorage = () =>
   JSON.parse(localStorage.getItem("db_client")) ?? []
@@ -24,13 +26,6 @@ const createClient = (client) => {
   setLocalStorage(dbClient)
 }
 
-const tempClient = {
-  name: "Irineu",
-  email: "teste@gmail.com",
-  tel: "123456789",
-  city: "Recife",
-}
-
 const isValidFields = () => {
   return document.getElementById("form").reportValidity()
 }
@@ -43,20 +38,90 @@ const clearFields = () => {
 //Interação com o usuário
 const saveClient = () => {
   if (isValidFields()) {
-    document.querySelector("button#save").setAttribute("data-dismiss", "modal")
+    btnSave.setAttribute("data-dismiss", "modal")
     const client = {
       name: document.getElementById("name").value,
       email: document.getElementById("email").value,
       tel: document.getElementById("tel").value,
       city: document.getElementById("city").value,
     }
-    createClient(client)
-    clearFields()
+    const index = document.getElementById("name").dataset.index
+    if (index === "new") {
+      createClient(client)
+      updateTable()
+      clearFields()
+    } else {
+      updateClient(index, client)
+      updateTable()
+    }
   } else {
-    document.querySelector("button#save").setAttribute("data-dismiss", "")
+    btnSave.setAttribute("data-dismiss", "")
     alert("Complete todos os campos para continuar!")
   }
 }
 
+const createRow = (client, index) => {
+  const newRow = document.createElement("tr")
+  newRow.innerHTML = `
+    <td>${client.name}</td>
+    <td>${client.email}</td>
+    <td>${client.tel}</td>
+    <td>${client.city}</td>
+    <td>
+      <button type="button" class="btn btn-success" id="edit-${index}" data-toggle="modal" data-target="#myModal">Editar</button>
+      <button type="button" class="btn btn-danger" id="delete-${index}">Excluir</button>
+    </td>
+  `
+  document.querySelector("table#tableClient>tbody").appendChild(newRow)
+}
+
+const clearTable = () => {
+  const rows = document.querySelectorAll("#tableClient>tbody tr")
+  rows.forEach((row) => row.parentNode.removeChild(row))
+}
+
+const updateTable = () => {
+  const dbClient = readClient()
+  clearTable()
+  dbClient.forEach(createRow)
+}
+
+const fillFields = (client) => {
+  document.getElementById("name").value = client.name
+  document.getElementById("email").value = client.email
+  document.getElementById("tel").value = client.tel
+  document.getElementById("city").value = client.city
+  document.getElementById("name").dataset.index = client.index
+}
+
+const editClient = (index) => {
+  const client = readClient()[index]
+  client.index = index
+  fillFields(client)
+}
+
+const editDelete = (event) => {
+  if (event.target.type === "button") {
+    const [action, index] = event.target.id.split("-")
+    if (action === "edit") {
+      editClient(index)
+    } else {
+      const client = readClient()[index]
+      const response = confirm(
+        `Você deseja realmente excluir o cliente ${client.name}`
+      )
+      if (response) {
+        deleteClient(index)
+        updateTable()
+      }
+    }
+  }
+}
+
+updateTable()
 //Eventos
-document.querySelector("button#save").addEventListener("click", saveClient)
+btnSave.addEventListener("click", saveClient)
+btnCancel.addEventListener("click", clearFields)
+document
+  .querySelector("#tableClient>tbody")
+  .addEventListener("click", editDelete)
