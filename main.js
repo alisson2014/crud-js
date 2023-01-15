3f64f7a970af7c9da65c995d472b9cae430c6cc5
alisson2014 <almeidaalisson2014@gmail.com>
2023-01-15 13:16:26 -0300

    adicionei novas funções

--- main.js
+++ main.js
@@ -4,6 +4,20 @@ const getLocalStorage = () =>
 const setLocalStorage = (dbClient) =>
   localStorage.setItem("db_client", JSON.stringify(dbClient))
 
+const readClient = () => getLocalStorage()
+
+const updateClient = (index, client) => {
+  const dbClient = readClient()
+  dbClient[index] = client
+  setLocalStorage(dbClient)
+}
+
+const deleteClient = (index) => {
+  const dbClient = readClient()
+  dbClient.splice(index, 1)
+  setLocalStorage(dbClient)
+}
+
 const createClient = (client) => {
   const dbClient = getLocalStorage()
   dbClient.push(client)
@@ -11,8 +25,24 @@ const createClient = (client) => {
 }
 
 const tempClient = {
-  name: "Alisson",
+  name: "Irineu",
   email: "teste@gmail.com",
   tel: "123456789",
-  city: "Balneário",
+  city: "Recife",
 }
+
+const isValidFields = () => {
+  return document.getElementById("form").reportValidity()
+}
+
+//Interação com o usuário
+const saveClient = () => {
+  if (isValidFields()) {
+    console.log("Cadastrando")
+  } else {
+    console.log("Erro, digite todos os campos")
+  }
+}
+
+//Eventos
+document.getElementById("save").addEventListener("click", saveClient)
